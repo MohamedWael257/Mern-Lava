3db41a94b93f2299babd8d23572e32685734ddbd
mohammed12w <116980264+mohammed12w@users.noreply.github.com>
2024-02-21 20:59:01 +0200

    first commit

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,5 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import dotnev from 'dotenv'
+dotnev.config()
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,5 +14,8 @@ export default defineConfig({
   //   },
   // },
   // },
+  define: {
+    'process.env': process.env
+  }
   // base: "/Mern-Lava/"
 })
