9b93eb80a73ade377b4ac20c7b73d267a404011e
mohammed12w <116980264+mohammed12w@users.noreply.github.com>
2024-02-21 16:09:55 +0200

    first commit

--- server/routes/booking.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const { booking, bookingData } = require("../controllers/booking.controller");
-const router = express.Router();
-
-router.post('/booking', booking);
-router.get('/bookingData', bookingData);
-
-module.exports = router
