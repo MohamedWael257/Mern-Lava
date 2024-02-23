e811f59ecbe22c6c58e08847ea7b30739881ea53
mohammed12w <116980264+mohammed12w@users.noreply.github.com>
2024-02-23 20:22:35 +0200

    first commit

--- client/src/components/auth/Login.jsx
+++ client/src/components/auth/Login.jsx
@@ -29,7 +29,7 @@ const Login = () => {
         else {
             // await axios.post('https://mern-lava-server.onrender.com/api/auth/login', { email, password })
             // await axios.post('https://mern-lava-server.vercel.app/api/auth/login', { email, password })
-            await axios.post(`${process.env.BASE_API_URL_HOST}/auth/login`, { email, password })
+            await axios.post(`http://localhost:5000/api/auth/login`, { email, password })
                 .then(res => {
                     console.log(res.data, "userRegister");
                     if (res.data.status == "ok") {
