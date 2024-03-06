cf7b22fce20398c81f68ed62a24f92ad6cc6a76a
mohammed12w <116980264+mohammed12w@users.noreply.github.com>
2024-03-06 19:42:28 +0200

    first commit

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,14 +5,14 @@ dotenv.config()
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3000,
-    // proxy: {
-    //   "/api": {
-    //     target: "http://localhost:5000",
-    //   },
-    // },
-  },
+  // server: {
+  // port: 3000,
+  // proxy: {
+  //   "/api": {
+  //     target: "http://localhost:5000",
+  //   },
+  // },
+  // },
   define: {
     'process.env': process.env
   },
